a66960f4b74003ab1bf0338ee34d9e570a2bc69e
Luke Wainhouse <lukewainhouse@hotmail.com>
2021-11-11 15:10:13 +0000

    LW: Initial Commit

---
+++ src/main/java/com/wainhouse/tta/TodoTaskApiApplication.java
@@ -0,0 +1,13 @@
+package com.wainhouse.tta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTaskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTaskApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wainhouse/tta/TodoTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wainhouse.tta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTaskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
